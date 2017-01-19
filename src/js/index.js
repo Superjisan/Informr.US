99456a7aaf681b2e72999ad1326ce0bfcf2f50ed
Jisan Zaman <jzaman@wesleyan.edu>
2017-01-18 21:53:30 -0500

    Add functionality for showing congressional reps, resetting functionality

--- src/js/index.js
+++ src/js/index.js
@@ -35,38 +35,86 @@ const validateAddress = () => {
 	}
 }
 
+const resetResults = () => {
+    $('.state-legislators-panels').empty();
+    $('.congress-legislators-panels').empty();
+}
+
 const getReps = address => {
+    resetResults();
 	const apiKey = 'AIzaSyAalrWHw-aemMa2n3Ou6T3isuVzeHtTBgI';
 	const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`;
 	$.get(url, ({ results }) => {
 		let location = _.get(results, '[0].geometry.location');
 		$.get(`/geolookup/${location.lat}&/${location.lng}`, data => {
-			$('.json').jsontree(JSON.stringify(data))
-			$('.result-json').removeClass('hidden');
+			// $('.json').jsontree(JSON.stringify(data))
+			// $('.result-json').removeClass('hidden');
             const stateLegislators = _.filter(data, legislator => {
-                return legislator.level === 'state';
+                return legislator.level === 'state' || !legislator.state_name;
             });
-            console.log(stateLegislators);
+            const congressLegislators =  _.filter(data, legislator => {
+                return legislator.level !== 'state' && legislator.state_name;
+            });
+
             stateLegislators.forEach(leg => {
-                $('.result-legislators-panels').append(generateLegislatorForState(leg));
-            })
+                $('.state-legislators-panels').append(generateLegislatorForState(leg));
+            });
+
+            congressLegislators.forEach(leg => {
+                $('.congress-legislators-panels').append(generateLegislatorsForCongress(leg));
+            });
+
 		})
 	});
 }
 
+const getPartyFull = partyAbbv => {
+    switch(partyAbbv) {
+        case 'D':
+            return 'Democratic';
+        case 'R':
+            return 'Republican';
+        case 'I':
+            return 'Independent';
+        default:
+            return 'N/A';
+    }
+};
+
+const generateLegislatorsForCongress = data => {
+    const html = `<h4 class="hidden">State Legislators</h4>
+    <div class="panel panel-default mt10">
+        <div class="panel-body row">
+            <div class="col-xs-12 col-sm-12">
+                <p class="legislator-name">
+                    <a href="${data.website}" target="_blank" >
+                        ${data.first_name} ${data.last_name}
+                    </a>
+                </p>
+                <p class="legislator-party">Party: ${getPartyFull(data.party)} </p>
+                <p class="legislator-state">State: ${data.state_name}</p>
+                <p class="legislator-chamber">Chamber: ${_.upperFirst(data.chamber)}</p>
+            </div>
+        </div>
+    </div>`;
+    return html;
+};
+
+
 const generateLegislatorForState = data => {
-    console.log(data);
-    const html = `<div class="panel panel-default mt10">
+    const html = `<h4 class="hidden">Congressional Legislators</h4>
+    <div class="panel panel-default mt10">
         <div class="panel-body row">
             <div class="legislator-img-col col-xs-3 col-sm-3">
                 <img class="leg-img" src="${data.photo_url}"/>
             </div>
             <div class="col-xs-9 col-sm-9">
-                <p class="legislator-name"> ${data.full_name}</p>
-                <p class="legislator-party">Party: ${data.party}</p>
-                <p class="legislator-state">State: ${data.state}</p>
-                <p class="legislator-level">Level: ${data.level}</p>
-                <p class="legislator-chamber">Chamber: ${data.chamber}</p>
+                <p class="legislator-name">
+                    <a href="${data.url}" target="_blank" >${data.full_name}</a>
+                </p>
+                <p class="legislator-party">Party: ${_.upperFirst(data.party)}</p>
+                <p class="legislator-state">State: ${data.state.toUpperCase()}</p>
+                <p class="legislator-chamber">Chamber: ${_.upperFirst(data.chamber)}</p>
             </div>
         </div>
     </div>`
