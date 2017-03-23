f58c11c52f4b0d97fe1c4a0489f230feb16ac69f
Jisan Zaman <Jzaman@wesleyan.edu>
2017-03-23 11:05:53 -0400

    Fix Issue #43 - Add VA state images (#44)
    * add va state images
    
    * add logic to get va image data
    
    * refactor representative cards to have button for phone call and have more of a vertical card UI
    
    * remove console.log

--- src/js/index.js
+++ src/js/index.js
@@ -72,7 +72,7 @@ const getGeocodeForAddress = address => {
 	const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
 	$.get(url, ({ results }) => {
 		let location = _.get(results, '[0].geometry.location');
-		getReps(location)
+		getReps(location);
 	});
 };
 
@@ -115,56 +115,59 @@ const getPartyFull = partyAbbv => {
 
 const generatePhoneForCongress = data => {
 	const phone = data.phone;
-	return `<p class="legislator-phone">
-		<strong>Phone:</strong>
-		<a href="tel:${phone.replace(/-/g, '')}">${phone}</a>
+    const repTitle = _.upperFirst(data.chamber) === 'Senate' ? 'Senator' : 'Representative';
+	return `<p class="text-center legislator-phone">
+		<a class="btn btn-default btn-block" href="tel:${phone.replace(/-/g, '')}">
+            <i class="fa fa-phone"></i>
+            Call ${repTitle} ${data.last_name}
+        </a>
 	</p>`;
 };
 
 const generateAddressForCongress = data => {
-	return `<p class="legislator-address">
+	return `<p class="text-center legislator-address">
 		<strong>Address:</strong>
 		${data.office}, Washington, DC 20003
 	</p>`;
-}
+};
 
 const generateSocialMediaForCongress = data => {
-	return `<p class="legislator-socialmedia">
+	return `<p class="text-center legislator-socialmedia">
 		${data.twitter_id ? `<a href="https://twitter.com/${data.twitter_id}" target="_blank">
 		<img src="../img/twitter.svg" class="social"/></a>&nbsp;&nbsp;` : ''}
 		${data.facebook_id ? `<a href="https://www.facebook.com/${data.facebook_id}" target="_blank">
 		<img src="../img/fb.svg" class="social"/></a>&nbsp;&nbsp;` : ''}
 		${data.youtube_id ? `<a href="https://www.youtube.com/user/${data.youtube_id}" target="_blank">
 		<img src="../img/youtube.svg" class="social"/></a>&nbsp;&nbsp;` : ''}
-	</p>`			
-}
+	</p>`;
+};
 
 const generateLegislatorsForCongress = data => {
 	const photoUrlHtml = data.photo_url ? `src="${data.photo_url}"` : '';
 	const html = `
 	<div class="panel panel-default mt10">
 		<div class="panel-body row">
-			<div class="legislator-img-col col-xs-4 col-sm-3">
-				<a href="#">
-					<img ${photoUrlHtml} class="leg-img img-thumbnail"/>
+			<div class="legislator-img-col col-xs-12 col-sm-12 text-center">
+				<a href="#" class="thumbnail">
+					<img ${photoUrlHtml}/>
 				</a>
 				${generateSocialMediaForCongress(data)}
 			</div>
-			<div class="col-xs-8 col-sm-9">
-				<p class="legislator-name">
+			<div class="col-xs-12 col-sm-12">
+				<p class="text-center legislator-name">
 					<a href="${data.website}" target="_blank" >
 						${data.first_name} ${data.last_name}
 					</a>
 				</p>
-				<p class="legislator-party">
+				<p class="text-center legislator-party">
 					<strong>Party:</strong> ${getPartyFull(data.party)} </p>
-				<p class="legislator-state">
+				<p class="text-center legislator-state">
 					<strong>State:</strong> ${data.state_name}</p>
-				<p class="legislator-chamber">
+				<p class="text-center legislator-chamber">
 					<strong>Chamber:</strong>
 					${_.upperFirst(data.chamber)}
 				</p>
-				${data.district ? `<p class="legislator-district">
+				${data.district ? `<p class="text-center legislator-district">
 					<strong>District:</strong> ${data.district}
 				</p>`: ''}
 				${generateAddressForCongress(data)}
@@ -191,24 +194,28 @@ const generatePhoneNumbersForStateLeg = data => {
 
 	const distOfficeHtml = `${
 		distOfficePhone ? `
-			<p class="legislator-district-phone">
-				<strong>District Office Phone:</strong>
-				<a href="tel:${distOfficePhone.replace(/-/g, '')}">${distOfficePhone}</a>
+			<p class="text-center legislator-district-phone">
+				<a class="btn btn-default btn-block" href="tel:${distOfficePhone.replace(/-/g, '')}">
+                    <i class="fa fa-phone"></i>
+                    Call District Office: ${distOfficePhone}
+                </a>
 			</p>` : ''
 	}`;
 
 	const capitolOfficeHtml = `${
 		capOfficePhone ? `
-			<p class="legislator-capitol-phone">
-				<strong>Capitol Office Phone:</strong>
-				<a href="tel:${capOfficePhone.replace(/-/g, '')}">${capOfficePhone}</a>
+			<p class="text-center legislator-capitol-phone">
+				<a class="btn btn-default btn-block" href="tel:${capOfficePhone.replace(/-/g, '')}">
+                    <i class="fa fa-phone"></i>
+                    Call Capitol Office ${capOfficePhone}
+                </a>
 			</p>` : ''
 	}`;
 
 	html = `${distOfficeHtml}${capitolOfficeHtml}`;
 
 	return html;
-}
+};
 
 const generateAddressForStateLeg = data => {
 	let html = '';
@@ -224,12 +231,12 @@ const generateAddressForStateLeg = data => {
 		capOfficeAddress = capitolOffice.address;
 	}
 
-	const distOfficeHtml = districtOffice ? `<p class="legislator-district-address">
+	const distOfficeHtml = districtOffice ? `<p class="text-center legislator-district-address">
 				<strong>District Office Address:</strong>
 				<span>${distOfficeAddress}</span>
 			</p>` : '';
 
-	const capitolOfficeHtml = capitolOffice ? `<p class="legislator-capitol-address">
+	const capitolOfficeHtml = capitolOffice ? `<p class="text-center legislator-capitol-address">
 				<strong>Capitol Office Address:</strong>
 				<span>${capOfficeAddress}</span>
 			</p>` : '';
@@ -243,30 +250,30 @@ const generateLegislatorForState = data => {
 	const html = `
 	<div class="panel panel-default mt10">
 		<div class="panel-body row">
-			<div class="legislator-img-col col-xs-4 col-sm-3">
+			<div class="legislator-img-col col-xs-12 col-sm-12 text-center">
 				<a href="#" class="thumbnail">
 					<img src="${data.photo_url}" alt="state_rep_image"/>
 				</a>
 			</div>
-			<div class="col-xs-8 col-sm-9">
-				<p class="legislator-name">
+			<div class="col-xs-12 col-sm-12">
+				<p class="text-center legislator-name">
 					<a href="${data.url}" target="_blank" >${data.full_name}</a>
 				</p>
-				<p class="legislator-party">
+				<p class="text-center legislator-party">
 					<strong>Party:</strong> ${_.upperFirst(data.party)}
 				</p>
-				<p class="legislator-state">
+				<p class="text-center legislator-state">
 					<strong>State:</strong> ${data.state.toUpperCase()}
 				</p>
-				<p class="legislator-chamber">
+				<p class="text-center legislator-chamber">
 					<strong>Chamber:</strong>
 					${data.chamber === 'upper' ? 'State Senate' : 'State Assembly'}
 				</p>
-				<p class="legislator-chamber">
+				<p class="text-center legislator-chamber">
 					<strong>District:</strong> ${data.district}
 				</p>
+                ${generateAddressForStateLeg(data)}
 				${generatePhoneNumbersForStateLeg(data)}
-				${generateAddressForStateLeg(data)}
 			</div>
 		</div>
 	</div>`;
